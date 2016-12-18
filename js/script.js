3f7d8d00271abc1e6bbe4e36e56ad808ec0d6acb
Naik, Priyanka <Priyanka.Naik@cdk.com>
2016-12-18 23:52:50 +0530

    Added show less and more for project details

--- js/script.js
+++ js/script.js
@@ -1,91 +1,24 @@
 $(document).ready(function() {
-    // $('.rating').css('display', 'none');
-    // $(".project-details").shorten();
+    $(".show-more").click(function () {
+        if($(".text").hasClass("show-more-height")) {
+            $(this).text("Show Less");
+        } else {
+            $(this).text("Show More");
+        }
+        $(".text").toggleClass("show-more-height");
+    });
 
-    // // $('.progress .progress-bar').css("width",
-    // //     function() {
-    // //         return $(this).attr("aria-valuenow") + "%";
-    // //     }
-    // // );
-    //
-    // $(".project-details").shorten({
-    //     "showChars" : 200
-    // });
-    //
-    //
-    // $(".project-details").shorten({
-    //     "showChars" : 150,
-    //     "moreText"	: "See More"
-    // });
-    //
-    // $(".project-details").shorten({
-    //     "showChars" : 50,
-    //     "moreText"	: "See More",
-    //     "lessText"	: "Less"
-    // });
-
-    // $('#skillJava').hover(function () {
-    //     $('#ratingJava').css('display', 'block');
-    //     $('#skillJava').css('text-decoration', 'underline');
-    // }, function () {
-    //     $('#ratingJava').css('display', 'none');
-    //     $('#skillJava').css('text-decoration', 'none');
-    // });
-    //
-    // $('#skillSBF').hover(function () {
-    //     $('#ratingSBF').css('display', 'block');
-    //     $('#skillSBF').css('text-decoration', 'underline');
-    // }, function () {
-    //     $('#ratingSBF').css('display', 'none');
-    //     $('#skillSBF').css('text-decoration', 'none');
-    // });
-    //
-    // $('#skillHTML').hover(function () {
-    //     $('#ratingHTML').css('display', 'block');
-    //     $('#skillHTML').css('text-decoration', 'underline');
-    // }, function () {
-    //     $('#ratingHTML').css('display', 'none');
-    //     $('#skillHTML').css('text-decoration', 'none');
-    // });
-    //
-    // $('#skillAngularJs').hover(function () {
-    //     $('#ratingAngularJs').css('display', 'block');
-    //     $('#skillAngularJs').css('text-decoration', 'underline');
-    // }, function () {
-    //     $('#ratingAngularJs').css('display', 'none');
-    //     $('#skillAngularJs').css('text-decoration', 'none');
-    // });
-    //
-    // $('#skillNodeJs').hover(function () {
-    //     $('#ratingNodeJs').css('display', 'block');
-    //     $('#skillNodeJs').css('text-decoration', 'underline');
-    // }, function () {
-    //     $('#ratingNodeJs').css('display', 'none');
-    //     $('#skillNodeJs').css('text-decoration', 'none');
-    // });
-    //
-    // $('#skillC').hover(function () {
-    //     $('#ratingC').css('display', 'block');
-    //     $('#skillC').css('text-decoration', 'underline');
-    // }, function () {
-    //     $('#ratingC').css('display', 'none');
-    //     $('#skillC').css('text-decoration', 'none');
-    // });
-    //
-    // $('#skillCpp').hover(function () {
-    //     $('#ratingCpp').css('display', 'block');
-    //     $('#skillCpp').css('text-decoration', 'underline');
-    // }, function () {
-    //     $('#ratingCpp').css('display', 'none');
-    //     $('#skillCpp').css('text-decoration', 'none');
-    // });
-    //
-    // $('#skillJavascript').hover(function () {
-    //     $('#ratingJavascript').css('display', 'block');
-    //     $('#skillJavascript').css('text-decoration', 'underline');
-    // }, function () {
-    //     $('#ratingJavascript').css('display', 'none');
-    //     $('#skillJavascript').css('text-decoration', 'none');
-    // });
+    $(".naik-electroman-project").hide();
+    $(".show-more-projects").click(function () {
+        if($(".text-project").hasClass("show-more-height-projects")) {
+            $(this).text("View Less");
+            $(".naik-electroman-project").show();
+        } else {
+            $(this).text("View More Projects");
+            $(".naik-electroman-project").hide();
+        }
+        $(".text-project").toggleClass("show-more-height-projects");
+        $(this).find('i').toggleClass('fa-caret-down fa-caret-up')
+    });
 
 });
